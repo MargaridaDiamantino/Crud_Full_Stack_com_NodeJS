5b8a73116fcc029d81289e2231289dec07f2c350
MargaridaDiamantino <margaridadiamantino18@gmail.com>
2024-01-06 23:04:19 +0100

    fix:testing gitignore

--- frontend/frontend/src/Componentes/Form.jsx
+++ frontend/frontend/src/Componentes/Form.jsx
@@ -1,8 +1,8 @@
-import React, { useRef,useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import { toast } from "react-toastify";
-const FormConteiner= styled.form `
+const FormConteiner = styled.form`
 display:flex;
 align-items:flex-end;
 gap:10px;
@@ -13,13 +13,13 @@ box-shadow:0px 0px 5px #ccc;
 border-radius:5px;
 
 `;
-const InputArea= styled.div`
+const InputArea = styled.div`
 display:flex;
 flex-direction: column;
 
 `;
 
-const Input= styled.input`
+const Input = styled.input`
 width:120px;
 padding:0 10px;
 border:1px solid #bbb;
@@ -39,93 +39,93 @@ heigth:42px;
 
 `;
 
-const Label= styled.label=``
+const Label = styled.label = ``
 
-const Form=({onEdit,setOnEdit,getUsers})=>{
-    const ref= useRef()
-    useEffect(()=>{
-      if(onEdit){
-        const user=ref.current
-        user.nome.value=onEdit.nome;
-        user.email.value=onEdit.email;
-        user.fone.value=onEdit.fone;
-        user.data_nascimento.value=onEdit.data_nascimento;
-      }
-    },[onEdit])
+const Form = ({ onEdit, setOnEdit, getUsers }) => {
+  const ref = useRef()
+  useEffect(() => {
+    if (onEdit) {
+      const user = ref.current
+      user.nome.value = onEdit.nome;
+      user.email.value = onEdit.email;
+      user.fone.value = onEdit.fone;
+      user.data_nascimento.value = onEdit.data_nascimento;
+    }
+  }, [onEdit])
 
-const handleSubmit =async(e)=>{
-  e.preventDefault()
-  const user = ref.current
-if(!user.nome.value||
-  !user.email.value||
-  !user.fone.value||
-  !user.data_nascimento.value){
-    return toast.warn("Preencha todos os campos")
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const user = ref.current
+    if (!user.nome.value ||
+      !user.email.value ||
+      !user.fone.value ||
+      !user.data_nascimento.value) {
+      return toast.warn("Preencha todos os campos")
+    }
+
+    if (onEdit) {
+      await axios
+        .put("http://localhost:8800/" + onEdit.id, {
+          nome: user.nome.value,
+          email: user.email.value,
+          fone: user.fone.value,
+          data_nascimento: user.data_nascimento.value
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data))
+    }
+
+    else {
+      await axios.post("http://localhost:8800", {
+        nome: user.nome.value,
+        email: user.email.value,
+        fone: user.fone.value,
+        data_nascimento: user.data_nascimento.value
+      })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data))
+
+    }
+    user.nome.value = ""
+    user.email.value = ""
+    user.fone.value = ""
+    user.data_nascimento.value = ""
+    setOnEdit(null)
+    getUsers()
   }
+  return (
+    <>
 
-  if(onEdit){
-    await axios
-    .put("http://localhost:8800/"+onEdit.id,{
-      nome:user.nome.value,
-      email:user.email.value,
-      fone:user.fone.value,
-      data_nascimento:user.data_nascimento.value
-    })
-    .then(({data})=>toast.success(data))
-    .catch(({data})=>toast.error(data))
-  }
 
-  else{
-    await axios.post("http://localhost:8800",{
-      nome:user.nome.value,
-      email:user.email.value,
-      fone:user.fone.value,
-      data_nascimento:user.data_nascimento.value
-    })
-    .then(({data})=>toast.success(data))
-    .catch(({data})=>toast.error(data))
-    
-  }
-  user.nome.value=""
-  user.email.value=""
-  user.fone.value=""
-  user.data_nascimento.value=""
-  setOnEdit(null)
-  getUsers()
-}
-    return(
-        <>
-        
-       
-        <FormConteiner ref={ref} onSubmit={handleSubmit} >
-           <InputArea>
-         <label>Nome</label>
-         <Input name="nome"></Input>
-          
-           </InputArea>
+      <FormConteiner ref={ref} onSubmit={handleSubmit} >
+        <InputArea>
+          <label>Nome</label>
+          <Input name="nome"></Input>
 
-           <InputArea>
-         <label>E-mail</label>
-         <Input name="email" type="email"></Input>
-          
-           </InputArea>
+        </InputArea>
 
-           <InputArea>
-         <label>Telefone</label>
-         <Input name="fone"></Input>
-          
-           </InputArea>
-           <InputArea>
-         <label>Data de nascimento</label>
-         <Input name="data_nascimento" type="date"></Input>
-          
-           </InputArea>
+        <InputArea>
+          <label>E-mail</label>
+          <Input name="email" type="email"></Input>
 
-            <Button type="submit">SALVAR</Button>
-           
-        </FormConteiner>
-       
-        </>
-    )
+        </InputArea>
+
+        <InputArea>
+          <label>Telefone</label>
+          <Input name="fone"></Input>
+
+        </InputArea>
+        <InputArea>
+          <label>Data de nascimento</label>
+          <Input name="data_nascimento" type="date"></Input>
+
+        </InputArea>
+
+        <Button type="submit">SALVAR</Button>
+
+      </FormConteiner>
+
+    </>
+  )
 }
 export default Form;
